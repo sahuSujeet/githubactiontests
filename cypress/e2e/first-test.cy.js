b2d4f63d022420db7c4604a48bd88ae8bfcff0b0
Sujeet Sahu <sujeetsahu@sujeets-mbp.in.ibm.com>
2024-06-11 17:36:27 +0530

    added base url

--- cypress/e2e/first-test.cy.js
+++ cypress/e2e/first-test.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
     it('passes', () => {
-      cy.visit('http://localhost:5173/');
+      cy.visit('/');
       cy.get("li").should("have.length",6);
     })
   })
